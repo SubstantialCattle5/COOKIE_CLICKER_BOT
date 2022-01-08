368ec04172ee134f023085f1253d2b6b163ffa2d
Nilay Nath Sharan <92233871+SubstantialCattle5@users.noreply.github.com>
2022-01-08 12:29:41 +0530

    fixing bugs(comma error)

--- main.py
+++ main.py
@@ -12,18 +12,18 @@ cookie = driver.find_element_by_id('cookie')
 # Upgrades
 upgrades_investment = Check()
 
-
+def money_converter(org_money) :
+    return org_money.replace(',' ,'')
 
 count = 0
 while True:
     count = count + 1
     cookie.click()
     money = driver.find_element_by_id('money')
-
+    money = money_converter(money.text)
 
     if count % 5 == 0:
-        current_money = money.text
-        upgrades_investment.value_check(saved=int(current_money))
+        upgrades_investment.value_check(saved=int(money))
         buy = driver.find_element_by_id(upgrades_investment.id)
         buy.click()
 
